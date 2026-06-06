eedd0862da941974de996c8257756ae09ee6a26f
Dev Bhatt <138993106+Prittstick22@users.noreply.github.com>
2026-06-06 17:03:00 +0100

    Time: 7 ms (34.69%) | Memory: 19.4 MB (86.97%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.py
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.py
@@ -5,13 +5,11 @@ class Solution:
         rightsum = [0]*size
         for i in range(1, size):
             leftsum[i] = nums[i-1] + leftsum[i-1]
-            #[10, 4, 8, 3]
-            #[0, 10, 14, 22]
+           
 
         for j in range(size-2,-1,-1):
             rightsum[j] = nums[j+1] + rightsum[j+1]
-            #[10, 4, 8, 3]
-            #[15, 11, 3, 0]
+           
         
         for x in range(0,size):
             leftsum[x] = abs(leftsum[x] - rightsum[x])
